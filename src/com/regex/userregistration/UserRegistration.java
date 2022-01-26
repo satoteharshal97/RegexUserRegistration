00835b25968aada0e35d5ab6298763d835374e83
satoteharshal97 <37376689+satoteharshal97@users.noreply.github.com>
2022-01-26 19:14:14 +0530

    Initial commit

---
+++ src/com/regex/userregistration/UserRegistration.java
@@ -0,0 +1,8 @@
+package com.regex.userregistration;
+
+public class UserRegistration {
+    
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java Regex");
+    }
+}
