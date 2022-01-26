24dcb410d7dbd07cf860eac5d847c4f104792655
satoteharshal97 <37376689+satoteharshal97@users.noreply.github.com>
2022-01-26 19:18:48 +0530

    Added - Check first name

--- src/com/regex/userregistration/UserRegistration.java
+++ src/com/regex/userregistration/UserRegistration.java
@@ -1,8 +1,25 @@
 package com.regex.userregistration;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
-    
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        UserRegistration regex  = new UserRegistration();
         System.out.println("Welcome to Java Regex");
+        System.out.println("Enter first name:");
+        String firstName = scanner.nextLine();
+        regex.checkFirstName(firstName);
+    }
+
+    private void checkFirstName(String firstName) {
+        boolean value = Pattern.matches("[A-Z]{1}[a-zA-Z]{3,}", firstName);
+        if (value) {
+            System.out.println("Valid Name : " + firstName);
+        } else {
+            System.out.println("Invalid Name: " + firstName);
+        }
     }
 }
