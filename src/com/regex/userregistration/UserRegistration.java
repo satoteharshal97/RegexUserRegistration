ffdbeb123f8e380122cfd84f17eb1edb171754b4
satoteharshal97 <37376689+satoteharshal97@users.noreply.github.com>
2022-01-26 19:36:50 +0530

    Added - Check email address

--- src/com/regex/userregistration/UserRegistration.java
+++ src/com/regex/userregistration/UserRegistration.java
@@ -9,12 +9,12 @@ public class UserRegistration {
     private final static UserRegistration regex = new UserRegistration();
 
     public static void main(String[] args) {
-
         System.out.println("Welcome to Java Regex");
         System.out.println("Choose an appropriate option: ");
         boolean check = true;
         while (check) {
-            System.out.println("Select:\n" + "1.Check First Name\n" + "2.Check Last Name\n" + "3.Exit\n");
+            System.out.println("Select:\n" + "1.Check First Name\n" + "2.Check Last Name\n"
+                    + "3.Check EmailId\n" + "4.Exit\n");
             int option = Integer.parseInt(scanner.nextLine());
             switch (option) {
                 case 1:
@@ -26,11 +26,24 @@ public class UserRegistration {
                     regex.checkName(scanner.nextLine());
                     break;
                 case 3:
+                    System.out.println("Enter emailId:");
+                    regex.checkEmailId(scanner.nextLine());
+                    break;
+                case 4:
                     check = false;
                     break;
                 default:
                     System.out.println("Compile Again!");
             }
+        }
+    }
+
+    private void checkEmailId(String email) {
+        boolean variable = Pattern.matches("^[a-z]+[._+]?[a-z_0-9]*[@][a-z_0-9]+[.][a-z]{2,4}[.]?[a-z]*$",email.toLowerCase());
+        if (variable) {
+            System.out.println("Valid email -->" + email);
+        } else {
+            System.out.println("Invalid email -->" + email);
         }
     }
 
